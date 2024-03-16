6697362d38862653008df38ea1696e894feda3ea
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2024-03-16 17:20:32 +0200

    Create Tiktok Business Campaign model

--- campaigns/models.py
+++ campaigns/models.py
@@ -13,3 +13,22 @@ class CrossroadsCampaign(models.Model):
     lander_visitors = models.IntegerField()
     lander_searches = models.IntegerField()
     revenue_events = models.IntegerField()
+
+
+class TiktokBusinessCampaign(models.Model):
+    identifier = models.CharField(max_length=128)
+    name = models.IntegerField()
+    advertiser_id = models.CharField(max_length=128)
+    video_play_actions = models.IntegerField()
+    video_watched_2s = models.IntegerField()
+    video_watched_6s = models.IntegerField()
+    video_views_p25 = models.IntegerField()
+    video_views_p50 = models.IntegerField()
+    video_views_p75 = models.IntegerField()
+    video_views_p100 = models.IntegerField()
+    cost_per_conversion = models.FloatField()
+    cpc = models.FloatField()
+    ctr = models.FloatField()
+    clicks = models.IntegerField()
+    spend = models.FloatField()
+    conversion = models.FloatField()
