e290079aa1753e5a84ac266b6638ff65ab27a9f3
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2024-03-17 10:57:43 +0200

    Create Crossroads keyword's unique constraint

--- keywords/models.py
+++ keywords/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 
 class CrossroadsKeyword(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["campaign_id", "lander_keyword", "date"],
+                name="crossroads_unique_keyword_for_date",
+            )
+        ]
+
     campaign_id = models.CharField(max_length=128)
     lander_keyword = models.CharField(max_length=128)
     clicks = models.IntegerField()
