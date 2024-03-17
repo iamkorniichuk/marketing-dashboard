bbd95c1f208cdee4d0c370bce1cafa45e900083c
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2024-03-17 10:56:18 +0200

    Add Crossroads keyword's date field

--- keywords/models.py
+++ keywords/models.py
@@ -5,6 +5,7 @@ class CrossroadsKeyword(models.Model):
     campaign_id = models.CharField(max_length=128)
     lander_keyword = models.CharField(max_length=128)
     clicks = models.IntegerField()
+    date = models.DateField()
 
     def __str__(self):
         return self.lander_keyword
