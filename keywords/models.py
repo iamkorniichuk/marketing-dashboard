b928b41e29269972fd23e3087f3956bb6fc249c8
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2024-03-17 10:48:35 +0200

    Create Crossroads keyword model

---
+++ keywords/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class CrossroadsKeyword(models.Model):
+    campaign_id = models.CharField(max_length=128)
+    lander_keyword = models.CharField(max_length=128)
+    clicks = models.IntegerField()
+
+    def __str__(self):
+        return self.lander_keyword
