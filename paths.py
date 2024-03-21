86b6c4f4220a7303ddfbf79df88653e4741b412a
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2024-03-21 18:13:47 +0200

    Create module for absolute configs' paths

---
+++ paths.py
@@ -0,0 +1,7 @@
+from marketing_dashboard.settings import BASE_DIR
+
+
+TIKTOK_BUSINESS_CONFIG = BASE_DIR / "tiktok_business.json"
+CROSSROADS_CONFIG = BASE_DIR / "crossroads.json"
+GOOGLE_ADS_CONFIG = BASE_DIR / "google_ads.yaml"
+KEYWORD_REQUEST_CONFIG = BASE_DIR / "keyword_request.json"
