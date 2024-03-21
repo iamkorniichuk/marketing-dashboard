69840b1c5c936ac55a1b510f45249ed021b396bd
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2024-03-21 15:31:13 +0200

    Create Region model's admin panel

--- regions/admin.py
+++ regions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from commons.admin import get_all_fieldnames
+from regions.models import Region
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = get_all_fieldnames(Region)
