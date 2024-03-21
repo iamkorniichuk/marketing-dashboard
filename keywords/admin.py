ac49996f827f1c1d4846b2f05d8b96d6f8d81489
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2024-03-21 18:02:28 +0200

    Create Google Ads keyword model's admin panel

--- keywords/admin.py
+++ keywords/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+
+from keywords.models import GoogleAdsKeyword
+
+
+@admin.register(GoogleAdsKeyword)
+class GoogleAdsKeywordAdmin(admin.ModelAdmin):
+    list_filter = ["text"]
+    list_display = ["text", "get_regions_names"]
+
+    def get_regions_names(self, obj):
+        regions_names = obj.regions.values_list("name", flat=True)
+        return ", ".join(regions_names)
+
+    get_regions_names.short_description = "Regions"
