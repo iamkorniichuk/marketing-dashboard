9d8efc79efd7b2e00191d77d1919cc1ef1013926
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2024-03-29 19:24:04 +0200

    Fix invalid tuple's type annotation

--- keywords/utils.py
+++ keywords/utils.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Tuple
 
 from django.db.models import QuerySet
 
@@ -7,7 +7,7 @@ from keywords.models import GoogleAdsKeyword
 
 def extract_regions_groups(
     keywords: QuerySet[GoogleAdsKeyword],
-) -> Dict[tuple[str], str]:
+) -> Dict[Tuple[str], str]:
     results = {}
     for obj in keywords.all():
         regions = tuple(obj.regions.values_list("id", flat=True))
