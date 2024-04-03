62249c5146714100d8621b9c463ff5c9fa737104
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2024-04-03 20:19:16 +0300

    Move configs to separate folder

--- paths.py
+++ paths.py
@@ -1,11 +1,13 @@
 from marketing_dashboard.settings import BASE_DIR
 
 
-TIKTOK_BUSINESS_CONFIG = BASE_DIR / "tiktok_business.json"
-CROSSROADS_CONFIG = BASE_DIR / "crossroads.json"
-GOOGLE_ADS_CONFIG = BASE_DIR / "google_ads.yaml"
-GOOGLE_SEARCH_CONFIG = BASE_DIR / "google_search.json"
-KEYWORD_REQUEST_CONFIG = BASE_DIR / "keyword_request.json"
-GOOGLE_CREDENTIALS = BASE_DIR / "google_credentials.json"
-OXYLABS_CONFIG = BASE_DIR / "oxylabs.json"
-CHAT_GPT_CONFIG = BASE_DIR / "chat_gpt.json"
+CONFIGS_DIR = BASE_DIR / "configs"
+
+TIKTOK_BUSINESS_CONFIG = CONFIGS_DIR / "tiktok_business.json"
+CROSSROADS_CONFIG = CONFIGS_DIR / "crossroads.json"
+GOOGLE_ADS_CONFIG = CONFIGS_DIR / "google_ads.yaml"
+GOOGLE_SEARCH_CONFIG = CONFIGS_DIR / "google_search.json"
+KEYWORD_REQUEST_CONFIG = CONFIGS_DIR / "keyword_request.json"
+GOOGLE_CREDENTIALS = CONFIGS_DIR / "google_credentials.json"
+OXYLABS_CONFIG = CONFIGS_DIR / "oxylabs.json"
+CHAT_GPT_CONFIG = CONFIGS_DIR / "chat_gpt.json"
