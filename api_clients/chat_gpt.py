b61cebb234222a7b37ecc6d1233bbb21bf8d0f11
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2024-04-07 10:43:28 +0300

    Create marketing type choice's request by ChatGPT

--- api_clients/chat_gpt.py
+++ api_clients/chat_gpt.py
@@ -1,6 +1,6 @@
 import json
 import re
-from typing import Iterable
+from typing import Dict, Iterable
 from openai import OpenAI
 
 from paths import CHAT_GPT_CONFIG
@@ -14,6 +14,45 @@ class ChatGptApiClient(metaclass=Singleton):
 
         self.client = OpenAI(api_key=self.credentials["api_key"])
 
+    def request_marketing_type_choice(self, keywords: Iterable[str]) -> Dict[str, str]:
+        response = self.client.chat.completions.create(
+            model="gpt-3.5-turbo-0125",
+            temperature=0,
+            frequency_penalty=0,
+            top_p=0,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful SEO assistant in B2B and B2C Commerce",
+                },
+                {
+                    "role": "user",
+                    "content": self.build_marketing_type_choice_request(
+                        ["video production", "audio editing", "listen music", "podcast"]
+                    ),
+                },
+                {
+                    "role": "assistant",
+                    "content": "1. video production - B2B\n2. audio editing - B2B\n3. listen music - B2C\n4. podcast - B2C",
+                },
+                {
+                    "role": "user",
+                    "content": self.build_marketing_type_choice_request(keywords),
+                },
+            ],
+        )
+        content = response.choices[0].message.content
+        return self.parse_marketing_type_choice_response(content)
+
+    def build_marketing_type_choice_request(self, keywords):
+        listing = ", ".join([f"`{self.preprocess_keyword(word)}`" for word in keywords])
+        return f"Choose best marketing fit (B2C/B2B) for each keyword: {listing}"
+
+    def parse_marketing_type_choice_response(self, response_message: str):
+        pattern = r"\s*(\w+(?:\s+\w+)*)\s*-\s*(\w+)"
+        match_groups = re.findall(pattern, response_message)
+        return {match[0]: match[1] for match in match_groups}
+
     def request_keyword_suggestions(
         self, example_keywords: Iterable[str], response_keywords_number=5
     ):
@@ -25,7 +64,7 @@ class ChatGptApiClient(metaclass=Singleton):
             messages=[
                 {
                     "role": "system",
-                    "content": "You are a helpful SEO assistant in B2C Commerce, designed to output JSON.",
+                    "content": "You are a helpful SEO assistant in B2C Commerce",
                 },
                 {
                     "role": "user",
