4522fd699f3c416979abbac660b43b562be82400
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2024-04-19 15:23:09 +0300

    Fix running webdriver after quit

--- commons/webdriver.py
+++ commons/webdriver.py
@@ -8,6 +8,7 @@ class DisplayWebdriver:
         self.is_linux = platform.system() == "Linux"
 
         options = ChromeOptions()
+        options.add_argument("--no-sandbox")
         options.add_argument("--ignore-ssl-errors=yes")
         options.add_argument("--ignore-certificate-errors")
         options.add_argument("--blink-settings=imagesEnabled=false")
@@ -28,6 +29,7 @@ class DisplayWebdriver:
         return self.webdriver
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        self.webdriver.close()
         self.webdriver.quit()
         if self.is_linux:
             self.display.stop()
